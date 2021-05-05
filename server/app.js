e91917b1f6b0a90df3eed344feb8af3418e3c5bc
Zafeer <zafeersyed15@gmail.com>
2021-05-04 17:57:11 -0700

    created random game objects

--- server/app.js
+++ server/app.js
@@ -22,3 +22,22 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 module.exports = app;
+
+//single page
+
+let game0= {title:"game0" , genre: "Action", rating: 5, link: "url", image:"image.png", description:"game" }
+let game1= {title:"game1" , genre: "Mystery", rating: 1, link: "url", image:"image.png", description:"game" }
+let game2= {title:"game2" , genre: "Sci-Fi", rating: 10, link: "url", image:"image.png", description:"game" }
+let game3= {title:"game3" , genre: "Fantasy", rating: 7, link: "url", image:"image.png", description:"game" }
+
+let gameList = {0: game0, 1: game1, 2: game2, 3: game3 }
+
+router.get('/single', function (res,req){ //request route?
+  res.send()      //send a random game here?
+})
+
+//need to make few routes
+//two seperate routes or 1 route that reads the query parameter 
+//fugure out if you want to split it into one route
+//to click on game in multi page, might need to get game by id
+//
