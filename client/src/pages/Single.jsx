d29df6c277d798bff6bb916169ac98854e08dc4e
Demind9 <76022445+Demind9@users.noreply.github.com>
2021-05-10 11:51:14 -0700

    Add files via upload

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,1 +1,41 @@
+import '../App.css';
+import './Single.css';
 
+function Single() {
+    return (
+      <div className="App">
+        <div className="Top-box">
+          <button className="list-button">
+            <b>List by genre</b>
+          </button>
+          <button className="random-button">
+            <b>Generate new</b>
+          </button>
+          <header className="Game-title">
+            Name right here
+          </header>
+          <text>
+            Should we even include a picture background?
+          </text>
+        </div>
+        <div className="Left-box">
+          <h1>link+description?</h1>
+        </div>
+        <div className="Right-box">
+          {Review(1)}
+          {Review(2)}
+          {Review(3)}
+        </div>
+      </div>
+    )
+}
+
+function Review(props) {
+  return(
+    <h2>
+      Review {props}
+    </h2>
+  )
+}
+
+export default Single;
