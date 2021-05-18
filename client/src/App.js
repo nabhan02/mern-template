67a5dbb13d84e0e4995dd522057892ca353a2802
Demind9 <76022445+Demind9@users.noreply.github.com>
2021-05-18 16:40:29 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Single from './pages/Single';
+import Home from './pages/Home';
+import Multi from './pages/Multi';
+
+import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 
 function App() {
   return (
-    <Single />
+    <Router>
+      {/*
+      <div className="linkHeader">
+        <nav>
+          <Link to="/">Home </Link>
+          <Link to="/single">Single </Link>
+          <Link to="/multi">Multi</Link>
+        </nav>
+        <br/>
+      </div>
+      */}
+      
+      
+      <Route path="/" exact component={Home} />
+      <Route path="/single" component={Single} />
+      <Route path="/multi" component={Multi} />
+    </Router>
+
+    
+    //<Single />
   )
   /*
   return (
